33085fbf685ee24b1c8a890db916141ce0244b4f
Tejprakash <67041377+Tej988@users.noreply.github.com>
2022-08-02 11:21:13 +0530

    Update AddBlog.js

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -23,7 +23,7 @@ const AddBlog = () => {
   }
 
  const sendRequest = async () => {
-    const res = await axios.post("https://xenon-blog.herokuapp.com//api/blog/add", {
+    const res = await axios.post("https://xenon-blog.herokuapp.com/api/blog/add", {
       title: inputs.title,
       description: inputs.description,
       image: inputs.image,
